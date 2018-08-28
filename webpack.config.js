2028ddc40e4d43eb9cfbea56d72b8af14564b14a
Verner Fortelius <verner@fortelius.net>
2018-08-28 22:18:05 +0300

    babelstuff

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 module.exports = {
     mode: 'production',
-    entry: ['./index.js'],
+    entry: ['regenerator-runtime/runtime', './index.js'],
     output: {
         path: path.resolve('lib'),
         filename: 'flexinets-react-authentication.js',
