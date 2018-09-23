1604e240a827f6dde2245856442c900e176e888a
Verner Fortelius <verner@fortelius.net>
2018-09-23 22:24:12 +0300

    stuff

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 module.exports = {
     mode: 'production',
-    entry: ['regenerator-runtime/runtime', './index.js'],
+    entry: ['./index.js'],
     output: {
         path: path.resolve('lib'),
         filename: 'flexinets-react-authentication.js',
@@ -22,4 +22,4 @@ module.exports = {
         'jwt-decode': 'jwt-decode',
         'qs': 'qs'
     }
-}
+};
